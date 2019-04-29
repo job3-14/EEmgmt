cb31baa570a4204d7ec892bee73088ed801887e6
job314 <job314job@pm.me>
2019-04-29 23:10:21 +0900

    ログインボタンを追加

--- Docker/x64/apache2/www/login.php
+++ Docker/x64/apache2/www/login.php
@@ -17,28 +17,31 @@
     <h2 class="mdl-card__title-text">ログイン</h2>
   </div>
   <div class="mdl-card__supporting-text">
+<p>入退室管理システムへログイン<p>
+
 
 <form action="#">
   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-    <input class="mdl-textfield__input" type="text" id="sample3">
+    <input class="mdl-textfield__input" type="text">
     <label class="mdl-textfield__label" for="sample3">ユーザー名</label>
   </div>
 </form>
 
 <form action="#">
   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-    <input class="mdl-textfield__input" type="text" id="sample3">
+    <input class="mdl-textfield__input" type="password" ">
     <label class="mdl-textfield__label" for="sample3">パスワード</label>
   </div>
 </form>
 
-
-
-  </div>
-  </div>
+<div class="c-login">
+<button class="mdl-button mdl-js-button mdl-button--raised ">
+  ログイン
+</button>
 </div>
 
-
+  </div>
+</div>
 
   </body>
 </html>
