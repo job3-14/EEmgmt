421b2e630ee1f58c708d1b11051dce64807fd45e
job314 <job314job@pm.me>
2019-04-29 22:33:31 +0900

    テキストフィールドの追加

--- Docker/x64/apache2/www/login.php
+++ Docker/x64/apache2/www/login.php
@@ -9,5 +9,36 @@
   </head>
   <body>
 
+
+
+
+<div class="demo-card-wide mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title">
+    <h2 class="mdl-card__title-text">ログイン</h2>
+  </div>
+  <div class="mdl-card__supporting-text">
+
+<form action="#">
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    <input class="mdl-textfield__input" type="text" id="sample3">
+    <label class="mdl-textfield__label" for="sample3">ユーザー名</label>
+  </div>
+</form>
+
+<form action="#">
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    <input class="mdl-textfield__input" type="text" id="sample3">
+    <label class="mdl-textfield__label" for="sample3">パスワード</label>
+  </div>
+</form>
+
+
+
+  </div>
+  </div>
+</div>
+
+
+
   </body>
 </html>
