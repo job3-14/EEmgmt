b207c5b4d981daae6b84400f19a23316cee8a328
job314 <job314job@pm.me>
2019-04-29 22:22:03 +0900

    login.phpの更新

--- Docker/x64/apache2/www/login.php
+++ Docker/x64/apache2/www/login.php
@@ -2,6 +2,10 @@
 <html>
   <head>
     <title>ログイン</title>
+    <link rel="stylesheet" href="./material/material.min.css">
+    <script src="./material/material.min.js"></script>
+    <link rel="stylesheet" href="./material/iconfont/material-icons.css">
+    <link rel="stylesheet" type="text/css" href="custom.css">
   </head>
   <body>
 
