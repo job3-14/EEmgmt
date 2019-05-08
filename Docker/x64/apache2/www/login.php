8e9ec1447e90c711d2066f59d1a0aa2bca3e40b7
job314 <job314job@pm.me>
2019-05-09 00:12:26 +0900

    バグの修正

--- Docker/x64/apache2/www/login.php
+++ Docker/x64/apache2/www/login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_regenerate_id(true); //セッション固定化攻撃対策
 
 require_once('db_setting.php');
 //エラーメッセージの定義
@@ -25,13 +26,11 @@ if (isset($_POST["user"])){
      $sql->execute();
   $password_hash =  $sql->fetchColumn();
   //データベース終了
-  echo $password_hash."<br>";
 
 
 if (password_verify($password, $password_hash)) {
     echo "認証成功";
     $_SESSION["user"] = $user;
-    session_regenerate_id(true); //セッションID再発行(固定化攻撃対策)
 } else {
     echo "ユーザー名またはパスワードが間違っています";
 }
