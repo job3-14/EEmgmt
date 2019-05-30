6aa93c5566ec8688b754f334e623b8b84755e782
job314 <job314job@pm.me>
2019-05-30 12:13:31 +0900

    shutdown.pyの作成

---
+++ raspberry/shutdown/shutdown.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# coding:utf-8
+import time
+import RPi.GPIO as GPIO
+import subprocess
+
+gpio_pin = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_pin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+while True:
+    GPIO.wait_for_edge(gpio_pin, GPIO.FALLING)
+    i = 0
+
+    for i in range(500):
+        button_st = GPIO.input(gpio_pin)
+        if button_st == 0:
+            if i >= 499:
+                subprocess.check_output(["sudo", "shutdown","-h","now"])
+                break
+        else:
+                break
+        time.sleep(0.01)
