ed9b4f17e1caef8c059f714e5d5beb9940e8e0e5
job314 <job314job@pm.me>
2019-06-03 00:21:56 +0900

    サービスユーザーsql追加

--- Docker/x64/db/initdb/eemgmt.sql
+++ Docker/x64/db/initdb/eemgmt.sql
@@ -6,3 +6,9 @@ id INTEGER  NOT NULL,
 username VARCHAR(100) NOT NULL,
 pass VARCHAR(300) NOT NULL,
 PRIMARY KEY (id));
+
+CREATE TABLE service_user(
+idm INTEGER NOT NULL,
+notice VARCHAR(30) NOT NULL,
+address VARCHAR(300),
+PRIMARY KEY (idm));
