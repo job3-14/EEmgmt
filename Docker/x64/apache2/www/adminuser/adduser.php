cc9f317884ed01b9ef8540e7b72620d7d41bc815
job314 <job314job@pm.me>
2019-06-03 10:45:33 +0900

    form設定追加

--- Docker/x64/apache2/www/adminuser/adduser.php
+++ Docker/x64/apache2/www/adminuser/adduser.php
@@ -45,7 +45,7 @@ header('Location: /login.php');
   </div>
   <div class="mdl-card__supporting-text">
 
-<form action="#" method="POST">
+<form action="./adduser2.php" method="POST">
   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
     <input class="mdl-textfield__input" type="text"  name="user">
     <label class="mdl-textfield__label">ユーザー名</label>
@@ -137,6 +137,9 @@ header('Location: /login.php');
 </label>
 </div>
 
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit">
+   入力内容確認
+</button>
 </form>
 
 
