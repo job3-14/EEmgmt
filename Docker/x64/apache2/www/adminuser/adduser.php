cba80f08bd5f3aa55bf927f7b8b0d557882e4c9d
job314 <job314job@pm.me>
2019-06-03 23:32:11 +0900

    チェックボックス値設定

--- Docker/x64/apache2/www/adminuser/adduser.php
+++ Docker/x64/apache2/www/adminuser/adduser.php
@@ -68,28 +68,28 @@ $_SESSION["errorMessages"]=array();
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-2">
-  <input type="checkbox" id="switch-2" class="mdl-switch__input" name="addcard" value="addcard">
+  <input type="checkbox" id="switch-2" class="mdl-switch__input" name="addcard" value=1>
   <span class="mdl-switch__label">カード登録</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-3">
-  <input type="checkbox" id="switch-3" class="mdl-switch__input" name="editcard" value="editcard">
+  <input type="checkbox" id="switch-3" class="mdl-switch__input" name="editcard" value=1>
   <span class="mdl-switch__label">カード編集・削除</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-4">
-  <input type="checkbox" id="switch-4" class="mdl-switch__input" name="sendnotice" value="sendnotice">
+  <input type="checkbox" id="switch-4" class="mdl-switch__input" name="sendnotice" value=1>
   <span class="mdl-switch__label">入退室通知手動送信</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-5">
-  <input type="checkbox" id="switch-5" class="mdl-switch__input" name="viewexit" value="viewexit">
+  <input type="checkbox" id="switch-5" class="mdl-switch__input" name="viewexit" value=1>
   <span class="mdl-switch__label">入退室履歴の閲覧</span>
 </label>
 </div>
@@ -99,47 +99,47 @@ $_SESSION["errorMessages"]=array();
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-6">
-  <input type="checkbox" id="switch-6" class="mdl-switch__input" name="viewloginlog" value="viewloginlog">
+  <input type="checkbox" id="switch-6" class="mdl-switch__input" name="viewloginlog" value=1>
   <span class="mdl-switch__label">管理者ログイン試行ログ閲覧</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-7">
-  <input type="checkbox" id="switch-7" class="mdl-switch__input" name="deletelog" value="deletelog">
+  <input type="checkbox" id="switch-7" class="mdl-switch__input" name="deletelog" value=1>
   <span class="mdl-switch__label">入退室履歴の削除</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-8">
-  <input type="checkbox" id="switch-8" class="mdl-switch__input" name="initialize" value="Initialize">
+  <input type="checkbox" id="switch-8" class="mdl-switch__input" name="initialize" value=1>
   <span class="mdl-switch__label">初期化操作</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-9">
-  <input type="checkbox" id="switch-9" class="mdl-switch__input" name="setmail" value="setmail">
+  <input type="checkbox" id="switch-9" class="mdl-switch__input" name="setmail" value=1>
   <span class="mdl-switch__label">メールサーバー設定操作</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-10">
-  <input type="checkbox" id="switch-10" class="mdl-switch__input" name="shutdown" value="shutdown">
+  <input type="checkbox" id="switch-10" class="mdl-switch__input" name="shutdown" value=1>
   <span class="mdl-switch__label">システム終了(シャットダウン)</span>
 </label>
 </div>
 
 <div class="c-switch">
 <label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-11">
-  <input type="checkbox" id="switch-11" class="mdl-switch__input" name="edituser" value="edituser">
+  <input type="checkbox" id="switch-11" class="mdl-switch__input" name="edituser" value=1>
   <span class="mdl-switch__label">管理ユーザーの追加・編集・削除</span>
 </label>
 </div>
 
-<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit">
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" value=1>
    入力内容確認
 </button>
 </form>
