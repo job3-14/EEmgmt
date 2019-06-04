1708ca98c726678763879619ad15d0e994c673f7
job314 <job314job@pm.me>
2019-06-05 00:28:38 +0900

    データベース接続設定

--- Docker/x64/apache2/www/adminuser/adduser3.php
+++ Docker/x64/apache2/www/adminuser/adduser3.php
@@ -35,6 +35,14 @@ $_SESSION["errorMessages"]=$errorMessages;
 header('Location: ./adduser_error.php');
 }
 
+try {
+  $pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
+}catch (Exception $e){
+  $errorMessages[] = "データベースエラーです"
+  $_SESSION["errorMessages"]=$errorMessages;
+  header('Location: ./adduser_error.php');
+}
+
 
 ?>
 
