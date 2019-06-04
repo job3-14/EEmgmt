bd0d914130b1197a9a005198eb24a75778a55e18
job314 <job314job@pm.me>
2019-06-04 23:00:15 +0900

    直接アクセスリダイレクト処理

--- Docker/x64/apache2/www/adminuser/adduser2.php
+++ Docker/x64/apache2/www/adminuser/adduser2.php
@@ -3,6 +3,9 @@ session_start();
 if (!isset($_SESSION["user"])){
 header('Location: /login.php');
 }
+if($_SERVER["REQUEST_METHOD"] != "POST"){
+  header('Location: ./adduser.php'); 
+}
 require_once('../db_setting.php');
 $errorMessages = array();
 $permission = array();
