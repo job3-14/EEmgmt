14e7281d98c0401e57723dfd14467e26f856dea1
job314 <job314job@pm.me>
2019-06-05 11:50:33 +0900

    SQL追加

--- Docker/x64/db/initdb/eemgmt.sql
+++ Docker/x64/db/initdb/eemgmt.sql
@@ -4,6 +4,16 @@ USE EEmgmt;
 CREATE TABLE login(
 username VARCHAR(100) NOT NULL,
 pass VARCHAR(300) NOT NULL,
+p-addcard INTEGER NOT NULL,
+p-editcard INTEGER NOT NULL,
+p-sendnotice INTEGER NOT NULL,
+p-viewexit INTEGER NOT NULL,
+p-viewloginlog INTEGER NOT NULL,
+p-deletelog INTEGER NOT NULL,
+p-initialize INTEGER NOT NULL,
+p-setmail INTEGER NOT NULL,
+p-shutdown INTEGER NOT NULL,
+p-edituser INTEGER NOT NULL,
 PRIMARY KEY (username));
 
 CREATE TABLE service_user(
