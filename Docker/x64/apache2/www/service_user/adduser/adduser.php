93f8327880182e235442b2341b5d5b876dfed384
job314 <job314job@pm.me>
2019-06-18 20:34:19 +0900

    ファイル追加

---
+++ Docker/x64/apache2/www/service_user/adduser/adduser.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])){
+header('Location: /login.php');
+}
+$_POST = array();
+$_SESSION["errorMessages"]=array();
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>入退室管理システム</title>
+    <link rel="stylesheet" href="/material/material.min.css">
+    <script src="/material/material.min.js"></script>
+    <link rel="stylesheet" href="/material/iconfont/material-icons.css">
+    <link rel="stylesheet" type="text/css" href="/custom.css">
+  </head>
+  <body>
+
+    <!-- Always shows a header, even in smaller screens. -->
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <!-- Title -->
+          <span class="mdl-layout-title">利用ユーザー新規登録ウィザード</span>
+          <!-- Add spacer, to align navigation to the right -->
+          <div class="mdl-layout-spacer"></div>
+          <!-- Navigation. We hide it in small screens. -->
+          <nav class="mdl-navigation mdl-layout--large-screen-only">
+          </nav>
+          <div class="logout">
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onclick="location.href='/service_user/adminuser.php'">
+             キャンセル
+          </button>
+          </div>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="page-content"><!-- Your content goes here -->
+
+
+<div class="c-add-card mdl-card mdl-shadow--4dp">
+  <div class="mdl-card__supporting-text">
+    新規登録する利用ユーザーの情報を入力してください。
+  </div>
+  <div class="mdl-card__supporting-text">
+
+<form action="./adduser2.php" method="POST">
+
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    <input class="mdl-textfield__input" type="text"  name="user">
+    <label class="mdl-textfield__label">ユーザー名</label>
+  </div>
+  <br>
+
+  <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" value=1>
+   入力内容確認
+ </button>
+
+</form>
+</div>
+</div>
+</div>
+      </main>
+    </div>
+
+
+
+  </body>
+</html>
