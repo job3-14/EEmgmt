34642fbae0ee27a92959eb241b62475757bf649b
job314 <job314job@pm.me>
2019-06-20 08:13:27 +0900

    エラー処理追加

--- Docker/x64/apache2/www/service_user/adduser/adduser2.php
+++ Docker/x64/apache2/www/service_user/adduser/adduser2.php
@@ -3,18 +3,31 @@ session_start();
 if (!isset($_SESSION["user"])){
 header('Location: /login.php');
 }
-$errorMessages = array();
+//$errorMessages = array();
 
-if($_POST["user"]=="" || $_POST["email"]=="" || $_POST["cardidm"]=="" || $_POST["sendMethod"]==""){
-  echo "error";
-}else{
-  echo "hello!";
+if($_POST["user"]==""){
+  $errorMessages[] = "ユーザー名を入力してください";
+}
+if($_POST["email"]==""){
+  $errorMessages[] = "メールアドレスを入力してください";
+}
+if($_POST["cardidm"]==""){
+  $errorMessages[] = "カードidmを入力してください";
+}
+if($_POST["sendMethod"]==""){
+  $errorMessages[] = "送信方法が指定されていません";
 }
 
-$_SESSION["addcard"]["user"] = $_POST["user"];
-$_SESSION["addcard"]["email"] = $_POST["email"];
-$_SESSION["addcard"]["cardidm"] = $_POST["cardidm"];
-$_SESSION["addcard"]["sendMethod"] = $_POST["sendMethod"];
+//$_SESSION["addcard"]["user"] = $_POST["user"];
+//$_SESSION["addcard"]["email"] = $_POST["email"];
+//$_SESSION["addcard"]["cardidm"] = $_POST["cardidm"];
+//$_SESSION["addcard"]["sendMethod"] = $_POST["sendMethod"];
 
+if(!is_null($errorMessages)){
+  $_SESSION["errorMessages"]=$errorMessages;
+  header('Location: ./adduser_error.php');
+}else{
+  echo "エラーなし";
+}
 
  ?>
