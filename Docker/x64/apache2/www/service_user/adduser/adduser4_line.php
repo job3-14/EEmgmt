184677918415b0fc818cb3bf0bdbc20cf0c307c9
job314 <job314job@pm.me>
2019-06-21 00:20:06 +0900

    ファイル追加

---
+++ Docker/x64/apache2/www/service_user/adduser/adduser4_line.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])){
+header('Location: /login.php');
+}
+
+if($_POST["password"]=="" || !isset($_POST["password"])){
+  $errorMessages[] = "パスワードを入力してください";
+}
+
+if(!is_null($errorMessages)){
+  $_SESSION["errorMessages"]=$errorMessages;
+  header('Location: ./adduser_error.php');
+}else{
+  $_SESSION["addcard"]["password"] = password_hash($_POST["password"], PASSWORD_DEFAULT);
+  header('Location: ./adduser5.php');
+}
+?>
