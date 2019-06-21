38a0b5cd1c165d0728c479d1bc9af9260882e6f1
job314 <job314job@pm.me>
2019-06-21 15:30:49 +0900

    ファイル追加

---
+++ Docker/x64/apache2/www/service_user/adduser/adduser4_email.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])){
+header('Location: /login.php');
+}
+
+if($_POST["email2"] && !filter_var($_POST["email2"], FILTER_VALIDATE_EMAIL)){
+  $errorMessages[] = "メールアドレスを正しく入力してください";
+}elseif($_POST["email3"] && !filter_var($_POST["email3"], FILTER_VALIDATE_EMAIL)){
+  $errorMessages[] = "メールアドレスを正しく入力してください";
+}elseif($_POST["email4"] && !filter_var($_POST["email4"], FILTER_VALIDATE_EMAIL)){
+  $errorMessages[] = "メールアドレスを正しく入力してください";
+}elseif($_POST["email5"] && !filter_var($_POST["email5"], FILTER_VALIDATE_EMAIL)){
+  $errorMessages[] = "メールアドレスを正しく入力してください";
+}
+
+if(!is_null($errorMessages)){
+  $_SESSION["errorMessages"]=$errorMessages;
+  header('Location: ./adduser_error.php');
+}else{
+  echo "hello!";
+}
+?>
