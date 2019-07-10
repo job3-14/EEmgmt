c462dda00f7b3628ba9b3a1a7057b7e5a19e0e8b
job314 <job314job@pm.me>
2019-07-11 02:58:22 +0900

    コマンドPHP追加

--- Docker/x64/apache2/www/system/system2.php
+++ Docker/x64/apache2/www/system/system2.php
@@ -8,17 +8,18 @@ if($_POST["check"]!=1){
   $errorMessages[] = "内容確認にチェックを入れてください";
 }
 
+$file = "/var/command/signalfile.py";
 if(!is_null($errorMessages)){
   $_SESSION["errorMessages"]=$errorMessages;
   header('Location: /adduser_error.php');
 }else{
   if($_POST["operete"]){
     if($_POST["operete"]=="shutdown"){
-      echo "shutdown";
+      $command = "Shutdown";
+      file_put_contents($file, $command);
     }elseif($_POST["operete"]=="restart"){
-      echo "restart";
-    }elseif($_POST["operete"]=="initialize"){
-      echo "initialize";
+      $command = "Reboot";
+      file_put_contents($file, $command);
     }
   }else{
     $errorMessages[] = "操作エラーです";
