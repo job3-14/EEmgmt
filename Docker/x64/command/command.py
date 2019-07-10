d824c6abc1b1bc64e0f63a7f9a1a8d94fb3785b9
job314 <job314job@pm.me>
2019-07-11 00:47:12 +0900

    コメント修正

--- Docker/x64/command/command.py
+++ Docker/x64/command/command.py
@@ -1,14 +1,14 @@
 import os
 import subprocess
 import time
-path = "./file/signalfile.py"  //ファイルパス指定
+path = "./file/signalfile.py"  #ファイルパス指定
 
-if os.path.exists(path):  //ファイルが存在した場合削除
+if os.path.exists(path):  #ファイルが存在した場合削除
     os.remove(path)
 
 while True:
     if os.path.exists(path):
-        from file import signalfile  //ファイル読み込み
+        from file import signalfile  #ファイル読み込み
         if signalfile.command == "Shutdown":
             subprocess.check_output(["shutdown","-h","now"])
         elif signalfile.command == "Reboot":
