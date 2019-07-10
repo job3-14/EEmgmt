3ef5e64a29ab55a9c9035a51ccb40924fde0db12
job314 <job314job@pm.me>
2019-07-11 00:18:29 +0900

    コマンドファイル読込先変更

--- command/command.py
+++ command/command.py
@@ -1,14 +1,14 @@
 import os
 import subprocess
 import time
-path = "./signalfile.py"
+path = "./file/signalfile.py"
 
 if os.path.exists(path):
     os.remove(path)
 
 while True:
     if os.path.exists(path):
-        import signalfile
+        from file import signalfile
         if signalfile.command == "Shutdown":
             subprocess.check_output(["shutdown","-h","now"])
         elif signalfile.command == "Reboot":
