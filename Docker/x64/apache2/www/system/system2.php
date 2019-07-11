0a7ff00890f8290cf19f674e56205180678a7023
job314 <job314job@pm.me>
2019-07-11 10:29:40 +0900

    書き込みファイルエラー修正

--- Docker/x64/apache2/www/system/system2.php
+++ Docker/x64/apache2/www/system/system2.php
@@ -8,18 +8,18 @@ if($_POST["check"]!=1){
   $errorMessages[] = "内容確認にチェックを入れてください";
 }
 
-$file = "/var/command/signalfile.py";
+$path = "/var/command/signalfile.py";
 if(!is_null($errorMessages)){
   $_SESSION["errorMessages"]=$errorMessages;
   header('Location: /adduser_error.php');
 }else{
   if($_POST["operete"]){
     if($_POST["operete"]=="shutdown"){
-      $command = "Shutdown";
-      file_put_contents($file, $command);
+      $command = "command=Shutdown";
+      file_put_contents($path, $command);
     }elseif($_POST["operete"]=="restart"){
-      $command = "Reboot";
-      file_put_contents($file, $command);
+      $command = "command=Reboot";
+      file_put_contents($path, $command);
     }
   }else{
     $errorMessages[] = "操作エラーです";
