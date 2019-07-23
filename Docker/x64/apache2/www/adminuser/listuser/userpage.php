3412b42e02d8d0c2cfaa459712e984a9e87f8e10
job314 <job314job@pm.me>
2019-07-23 18:53:34 +0900

    個別ページ作成

---
+++ Docker/x64/apache2/www/adminuser/listuser/userpage.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])){
+header('Location: /login.php');
+}
+$_SESSION["errorMessages"]= array();
+include($_SERVER['DOCUMENT_ROOT'] . '/db_setting.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/menu_load.php');
+$username = $_GET["username"];
+
+try {
+    $pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
+    $sql  = $pdo->prepare("SELECT EXISTS(SELECT username FROM login WHERE username = ?)");
+    $sql->bindValue(1,$username);
+    $sql->execute();
+    $result=  $sql->fetchColumn();
+}catch (Exception $e){
+  $operateErrorMessages[] = "データベース接続エラーです";
+}
+
+if($result==0){
+  $operateErrorMessages[] = "操作エラーです";
+}
+
+if(isset($operateErrorMessages)){
+  $_SESSION["errorMessages"]=$operateErrorMessages;
+  header('Location: /operate_error.php');
+}
+?>
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>入退室管理システム</title>
+    <link rel="stylesheet" href="/material/material.min.css">
+    <script src="/material/material.min.js"></script>
+    <link rel="stylesheet" href="/material/iconfont/material-icons.css">
+    <link rel="stylesheet" type="text/css" href="/custom.css"> </head>
+  <body>
+    <!-- The drawer is always open in large screens. The header is always shown,
+      even in small screens. -->
+    <?php
+      $menuName = "管理者管理ページ";
+      menuload($menuName);
+      ?>
+      <main class="mdl-layout__content">
+        <div class="page-content">
+          <!-- Your content goes here -->
+          <div class="c-card-padding">
+            <div class="c-large-card mdl-card mdl-shadow--4dp">
+              <div class="mdl-card__supporting-text"> 管理ユーザー情報 </div>
+              <div class="mdl-card__supporting-text">
+                <h5>ユーザー名:<?php echo $username; ?></h5>
+
+               </div>
+            </div>
+          </div>
+      </main>
+      </div>
+  </body>
+  </html>
