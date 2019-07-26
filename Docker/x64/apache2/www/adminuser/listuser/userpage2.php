08943de3cd9393d6a66bf157336cff748e8df472
job314 <job314job@pm.me>
2019-07-27 01:36:04 +0900

    SQL変更

--- Docker/x64/apache2/www/adminuser/listuser/userpage2.php
+++ Docker/x64/apache2/www/adminuser/listuser/userpage2.php
@@ -6,18 +6,6 @@ header('Location: /login.php');
 $_SESSION["errorMessages"]= array();
 include($_SERVER['DOCUMENT_ROOT'] . '/db_setting.php');
 
-try {
-  if(!$_POST["user"]==""){
-    $pdo=new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
-    $sql=$pdo->prepare("SELECT EXISTS(SELECT username FROM login WHERE username = ?)");
-    $sql->bindValue(1,$_POST["user"]);
-    $sql->execute();
-    $result=$sql->fetchColumn();
-  }
-}catch (Exception $e){
-  $errorMessages[]="データベース接続エラーです"
-}
-
 if (isset($_POST["user"])){
   if($_POST["user"]==""){
     $errorMessages[] = "ユーザー名を入力してください";
@@ -25,7 +13,7 @@ if (isset($_POST["user"])){
   if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST["user"]) && !$_POST["user"]==""){
     $errorMessages[] = "ユーザー名は半角文字数字のみが使用できます";
   }
-  if($result==1){
+  if( && $result==1){
     $errorMessages[] = "このユーザー名は既に使用されている為使えません。別のユーザー名をご利用ください";
   }
   $_SESSION["errorMessages"]=$errorMessages;
@@ -40,21 +28,20 @@ if(isset($errorMessages)){
 }
 
 try {
-  $sql=$pdo->prepare("INSERT INTO login (username,addcard,editcard,sendnotice,viewexit,viewloginlog,deletelog,initialize,setmail,shutdown,edituser) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);");
-  $sql->bindValue(1,$_POST["adduser"]);
-  $sql->bindValue(2,$_POST["addcard"]);
-  $sql->bindValue(3,$_POST["editcard"]);
-  $sql->bindValue(4,$_POST["sendnotice"]);
-  $sql->bindValue(5,$_POST["viewexit"]);
-  $sql->bindValue(6,$_POST["viewloginlog"]);
-  $sql->bindValue(7,$_POST["deletelog"]);
-  $sql->bindValue(8,$_POST["initialize"]);
-  $sql->bindValue(9,$_POST["setmail"]);
-  $sql->bindValue(10,$_POST["shutdown"]);
-  $sql->bindValue(11,$_POST["edituser"]);
+  $pdo=new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
+  $sql=$pdo->prepare("INSERT INTO login (addcard,editcard,sendnotice,viewexit,viewloginlog,deletelog,initialize,setmail,shutdown,edituser) VALUES (?,?,?,?,?,?,?,?,?,?,?,?);");
+  $sql->bindValue(1,$_POST["addcard"]);
+  $sql->bindValue(2,$_POST["editcard"]);
+  $sql->bindValue(3,$_POST["sendnotice"]);
+  $sql->bindValue(4,$_POST["viewexit"]);
+  $sql->bindValue(5,$_POST["viewloginlog"]);
+  $sql->bindValue(6,$_POST["deletelog"]);
+  $sql->bindValue(7,$_POST["initialize"]);
+  $sql->bindValue(8,$_POST["setmail"]);
+  $sql->bindValue(9,$_POST["shutdown"]);
+  $sql->bindValue(10,$_POST["edituser"]);
   $sql->execute();
-  }
 }catch (Exception $e){
-  $errorMessages[]="データベース接続エラーです"
+  $errorMessages[]="データベース接続エラーです";
 }
 ?>
