045ee4282f0ce7bc0e0eedd67da45be2b856e7e6
job314 <job314job@pm.me>
2019-07-31 01:34:10 +0900

    flask基本プログラム追加

---
+++ Docker/x64/python3/flask.py
@@ -0,0 +1,10 @@
+#from flask import Flask, request, jsonify
+from flask import Flask, request, json
+app = Flask(__name__)
+@app.route('/sendMessage',methods=["POST"])
+def sendMessage():
+    postData=request.get_data()
+    postData=json.loads(postData)
+    print(postData["name"])
+    return request.get_data()
+app.run()
