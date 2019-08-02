97176b562125944bb461854bbf206e8992148c10
job314 <job314job@pm.me>
2019-08-02 16:59:29 +0900

    デバッグ削除

--- Docker/x64/apache2/www/sendmessage/enter.php
+++ Docker/x64/apache2/www/sendmessage/enter.php
@@ -85,5 +85,6 @@ curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 $result = curl_exec($ch);
 curl_close($ch);
-echo $data;
+
+header('Location: ./done.php?idm='.$idm);
 ?>
