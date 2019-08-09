c609fe09d486283459310f6a14181041648089ca
job314 <job314job@pm.me>
2019-08-09 23:20:32 +0900

    変数設定

--- Docker/x64/nginx/html/sendmessage/exit.php
+++ Docker/x64/nginx/html/sendmessage/exit.php
@@ -17,15 +17,6 @@ try {
   $operateErrorMessages[] = "データベース接続エラーです";
 }
 
-try {
-    $pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
-    $sql  = $pdo->prepare("SELECT * FROM setting");
-    $sql->execute();
-    $setting = $sql->fetchAll();
-}catch (Exception $e){
-  $operateErrorMessages[] = "データベース接続エラーです";
-}
-
 $data = date("Y-m-d H:i"); #日時取得
 try {
   $pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
@@ -68,9 +59,9 @@ if($user[0]["notice"]=="slack"){
 if($user[0]["notice"]=="email"){
   $jsonList=array("method"=>"email");
   $jsonList["subject"]="退室通知";
-  $jsonList["fromEmail"]=$setting[0]["fromEmail"];
-  $jsonList["mailUserid"]=$setting[0]["mailUserid"];
-  $jsonList["mailPassword"]=$setting[0]["mailPassword"];
+  $jsonList["fromEmail"]=getenv("FROM_EMAIL");
+  $jsonList["mailUserid"]=getenv("ID_EMAIL");
+  $jsonList["mailPassword"]=getenv("PASS_EMAIL");
   if($user[0]["address1"]){
     $jsonList["address1"]=$user[0]["address1"];
   }
@@ -90,7 +81,7 @@ if($user[0]["notice"]=="email"){
 
 if($user[0]["notice"]=="line"){
   $jsonList=array("method"=>"line");
-  $jsonList["lineToken"]=$setting[0]["lineToken"];
+  $jsonList["lineToken"]=getenv("LINEAPI_TOKEN");
   $jsonList["userid"]=$user[0]["address1"];
 }
 
