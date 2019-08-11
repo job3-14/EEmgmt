b497caa13840154400025faa212faac76b401077
job314 <job314job@pm.me>
2019-08-11 11:27:37 +0900

    変数変更

--- Docker/x64/linereply/api.py
+++ Docker/x64/linereply/api.py
@@ -16,8 +16,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.environ.get('LINEAPI_TOKEN')
-channel_access_token = os.environ.get('LINEAPI_SECRET')
+channel_secret = os.environ.get('LINEAPI_SECRET')
+channel_access_token = os.environ.get('LINEAPI_TOKEN')
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
