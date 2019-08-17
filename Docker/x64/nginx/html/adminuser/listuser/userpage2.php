7bbc51f8c4d3e5065fd131812c30d243d54c42bf
job314 <job314job@pm.me>
2019-08-18 05:40:06 +0900

    SQL修正

--- Docker/x64/nginx/html/adminuser/listuser/userpage2.php
+++ Docker/x64/nginx/html/adminuser/listuser/userpage2.php
@@ -18,27 +18,19 @@ $permisson["addcard"]=conversion($_POST["addcard"]);
 $permisson["editcard"]=conversion($_POST["editcard"]);
 $permisson["sendnotice"]=conversion($_POST["sendnotice"]);
 $permisson["viewexit"]=conversion($_POST["viewexit"]);
-$permisson["viewloginlog"]=conversion($_POST["viewloginlog"]);
-$permisson["deletelog"]=conversion($_POST["deletelog"]);
-$permisson["initialize"]=conversion($_POST["initialize"]);
-$permisson["setmail"]=conversion($_POST["setmail"]);
 $permisson["shutdown"]=conversion($_POST["shutdown"]);
 $permisson["edituser"]=conversion($_POST["edituser"]);
 
 try {
   $pdo=new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
-  $sql=$pdo->prepare("UPDATE login SET addcard=?, editcard=?, sendnotice=?, viewexit=?, viewloginlog=?, deletelog=?, initialize=?, setmail=?, shutdown=?, edituser=? WHERE username=?");
+  $sql=$pdo->prepare("UPDATE login SET addcard=?, editcard=?, sendnotice=?, viewexit=?, shutdown=?, edituser=? WHERE username=?");
   $sql->bindValue(1,$permisson["addcard"]);
   $sql->bindValue(2,$permisson["editcard"]);
   $sql->bindValue(3,$permisson["sendnotice"]);
   $sql->bindValue(4,$permisson["viewexit"]);
-  $sql->bindValue(5,$permisson["viewloginlog"]);
-  $sql->bindValue(6,$permisson["deletelog"]);
-  $sql->bindValue(7,$permisson["initialize"]);
-  $sql->bindValue(8,$permisson["setmail"]);
-  $sql->bindValue(9,$permisson["shutdown"]);
-  $sql->bindValue(10,$permisson["edituser"]);
-  $sql->bindValue(11,$_POST["username"]);
+  $sql->bindValue(5,$permisson["shutdown"]);
+  $sql->bindValue(6,$permisson["edituser"]);
+  $sql->bindValue(7,$_POST["username"]);
   $sql->execute();
 }catch (Exception $e){
   $errorMessages[]="データベース接続エラーです";
