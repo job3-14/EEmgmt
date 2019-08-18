399db631584d344f653f7fb8d5959864a88a5314
job314 <job314job@pm.me>
2019-08-18 12:12:12 +0900

    関数名変更

--- Docker/x64/nginx/html/permission.php
+++ Docker/x64/nginx/html/permission.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION["user"])){
 header('Location: /login.php');
 }
 
-function permission($type){
+function permission_redirect($type){
   include($_SERVER['DOCUMENT_ROOT'] . '/db_setting.php');
   try {
       $pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
