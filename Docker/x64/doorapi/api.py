1a8150c5b3b77c721f2831e608efdcd63580fa6b
job314 <job314job@pm.me>
2019-08-19 01:50:13 +0900

    SQL接続設定追加

--- Docker/x64/doorapi/api.py
+++ Docker/x64/doorapi/api.py
@@ -7,7 +7,18 @@ mysql_password = os.environ.get('MYSQL_PASSWORD')
 
 @app.route('/')
 def index():
-    pass
+    #データベース接続開始##################
+    conn = mysql.connector.connect(
+    	host='db',
+    	port='3306',
+    	user='root',
+    	password=mysql_password,
+    	database='EEmgmt'
+    )
+    conn.ping(reconnect=True) #自動再接続
+    cur = conn.cursor() #操作用カーソルオブジェクト作成
+    #######################################
+    
     return "OK"
 
 
