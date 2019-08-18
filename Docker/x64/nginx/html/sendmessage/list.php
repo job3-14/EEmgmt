b6a367d2d3e45e83cf488686fe811f4448fcca82
job314 <job314job@pm.me>
2019-08-18 12:17:44 +0900

    権限追加(手動送信)

--- Docker/x64/nginx/html/sendmessage/list.php
+++ Docker/x64/nginx/html/sendmessage/list.php
@@ -6,6 +6,12 @@ header('Location: /login.php');
 $_SESSION["errorMessages"]= array();
 include($_SERVER['DOCUMENT_ROOT'] . '/db_setting.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/menu_load.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/permission.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/permission.php');
+//権限確認
+permission_redirect("sendnotice");
+//権限確認
+permission_redirect("sendnotice");
 
 try {
     if($_GET["seach"]){
