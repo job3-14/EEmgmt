5073755e850821e268858a40bdad04638fda5a5e
job314 <job314job@pm.me>
2019-08-21 02:30:47 +0900

    クラス化

--- doorLock/servo.py
+++ doorLock/servo.py
@@ -1,16 +1,16 @@
 import wiringpi as pi
 import time
-servo_pin = 18
-CYCLE = 20
-RANGE = 2000
-
-clock = int( 19.2 / float(RANGE) * CYCLE * 1000 )
-
-pi.wiringPiSetupGpio()
-pi.pinMode( servo_pin, pi.GPIO.PWM_OUTPUT )
-pi.pwmSetMode( pi.GPIO.PWM_MODE_MS )
-pi.pwmSetRange( RANGE )
-pi.pwmSetClock( clock )
+class Door:
+    def __init__():
+        self.servo_pin = 18
+        self.CYCLE = 20
+        self.RANGE = 2000
+        self.clock = int( 19.2 / float(RANGE) * CYCLE * 1000 )
+        self.pi.wiringPiSetupGpio()
+        self.pi.pinMode( servo_pin, pi.GPIO.PWM_OUTPUT )
+        self.pi.pwmSetMode( pi.GPIO.PWM_MODE_MS )
+        self.pi.pwmSetRange( RANGE )
+        self.pi.pwmSetClock( clock )
 
 move_deg = int(150)
 pi.pwmWrite( servo_pin, move_deg )
