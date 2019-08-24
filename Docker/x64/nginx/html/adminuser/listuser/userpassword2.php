c47babfa20955f73dc1921801cf58eb27b753d2e
job314 <job314job@pm.me>
2019-08-25 00:24:57 +0900

    整形/password2

--- Docker/x64/nginx/html/adminuser/listuser/userpassword2.php
+++ Docker/x64/nginx/html/adminuser/listuser/userpassword2.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION["user"])){
-header('Location: /login.php');
-exit;
+if (!isset($_SESSION["user"])) {
+    header('Location: /login.php');
+    exit;
 }
 include($_SERVER['DOCUMENT_ROOT'] . '/db_setting.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/permission.php');
@@ -12,43 +12,42 @@ $username=$_POST["username"];
 $password1=$_POST["password1"];
 $password2=$_POST["password2"];
 
-if($username==$password1){
-  $errorMessages[] ="ユーザーとパスワードは一致してはいけません。違う組み合わせにしてください";
+if ($username==$password1) {
+    $errorMessages[] ="ユーザーとパスワードは一致してはいけません。違う組み合わせにしてください";
 }
-if($password1==""){
-  $errorMessages[] = "パスワードを入力してください";
-}elseif($password2){
-  $errorMessages[] = "パスワードを2度入力してください";
-}elseif($password1==$password2){
-  $errorMessages[] = "パスワードが一致していません";
+if ($password1=="") {
+    $errorMessages[] = "パスワードを入力してください";
+} elseif ($password2) {
+    $errorMessages[] = "パスワードを2度入力してください";
+} elseif ($password1==$password2) {
+    $errorMessages[] = "パスワードが一致していません";
 }
-if(strlen($password1)<6 || strlen($password1)>=50){
-  $errorMessages[] = "パスワードは6文字から50文字で入力してください。";
+if (strlen($password1)<6 || strlen($password1)>=50) {
+    $errorMessages[] = "パスワードは6文字から50文字で入力してください。";
 }
-if(isset($errorMessages)){
-  $_SESSION["errorMessages"]=$errorMessages;
-  header('Location: /normal_error.php');
-  exit;
+if (isset($errorMessages)) {
+    $_SESSION["errorMessages"]=$errorMessages;
+    header('Location: /normal_error.php');
+    exit;
 }
 
 $password=password_hash($password, PASSWORD_DEFAULT);
 
 try {
-  $pdo=new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
-  $sql=$pdo->prepare("UPDATE login SET pass=? WHERE username=?");
-  $sql->bindValue(1,$password);
-  $sql->bindValue(2,$username);
-  $sql->execute();
-}catch (Exception $e){
-  $operateErrorMessages[]="データベース接続エラーです";
+    $pdo=new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4', $DB_USER, $DB_PASS);
+    $sql=$pdo->prepare("UPDATE login SET pass=? WHERE username=?");
+    $sql->bindValue(1, $password);
+    $sql->bindValue(2, $username);
+    $sql->execute();
+} catch (Exception $e) {
+    $operateErrorMessages[]="データベース接続エラーです";
 }
 
-if(isset($operateErrorMessages)){
-  $_SESSION["errorMessages"]=$operateErrorMessages;
-  header('Location: /operate_error.php');
-  exit;
+if (isset($operateErrorMessages)) {
+    $_SESSION["errorMessages"]=$operateErrorMessages;
+    header('Location: /operate_error.php');
+    exit;
 }
 
 header('Location: ./userpassword3.php?username='.$username);
 exit;
-?>
