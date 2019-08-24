5b62022148f142dee92bca5b359111ae81deebbc
job314 <job314job@pm.me>
2019-08-24 11:33:48 +0900

    send/manual

--- Docker/x64/nginx/html/sendmessage/manual.php
+++ Docker/x64/nginx/html/sendmessage/manual.php
@@ -4,7 +4,6 @@ if (!isset($_SESSION["user"])){
 header('Location: /login.php');
 exit;
 }
-$_SESSION["errorMessages"]= array();
 include($_SERVER['DOCUMENT_ROOT'] . '/db_setting.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/permission.php');
 //権限確認
