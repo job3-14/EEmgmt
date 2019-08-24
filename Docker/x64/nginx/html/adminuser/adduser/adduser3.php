b549d794295aa7182cb272fbb7fddb5c5ad0bee7
job314 <job314job@pm.me>
2019-08-25 00:17:32 +0900

    整形/add3

--- Docker/x64/nginx/html/adminuser/adduser/adduser3.php
+++ Docker/x64/nginx/html/adminuser/adduser/adduser3.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-if (!isset($_SESSION["user"])){
-header('Location: /login.php');
-exit;
+if (!isset($_SESSION["user"])) {
+    header('Location: /login.php');
+    exit;
 }
-if ($_SESSION["adduser_status"] !== 1){
-header('Location: ./adduser.php');
-exit;
+if ($_SESSION["adduser_status"] !== 1) {
+    header('Location: ./adduser.php');
+    exit;
 }
 
 include($_SERVER['DOCUMENT_ROOT'] . '/db_setting.php');
@@ -15,104 +15,103 @@ include($_SERVER['DOCUMENT_ROOT'] . '/permission.php');
 permission_redirect("edituser");
 
 try {
-  $pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4',$DB_USER, $DB_PASS);
-}catch (Exception $e){
-  $errorMessages[] = "データベースエラーです";
-  $_SESSION["errorMessages"]=$errorMessages;
-  header('Location: /normal_error.php');
-  exit;
+    $pdo = new PDO('mysql:host='.$DB_HOST.';dbname='.$DB_NAME.';charset=utf8mb4', $DB_USER, $DB_PASS);
+} catch (Exception $e) {
+    $errorMessages[] = "データベースエラーです";
+    $_SESSION["errorMessages"]=$errorMessages;
+    header('Location: /normal_error.php');
+    exit;
 }
 
 try {
-  if(!$_SESSION["adduser"]==""){
-    $sql  = $pdo->prepare("SELECT EXISTS(SELECT username FROM login WHERE username = ?)");
-    $sql->bindValue(1,$_SESSION["adduser"]);
-    $sql->execute();
-    $result=  $sql->fetchColumn();
-    $dberror=0;
-  }
-}catch (Exception $e){
-  $dberror=1;
+    if (!$_SESSION["adduser"]=="") {
+        $sql  = $pdo->prepare("SELECT EXISTS(SELECT username FROM login WHERE username = ?)");
+        $sql->bindValue(1, $_SESSION["adduser"]);
+        $sql->execute();
+        $result=  $sql->fetchColumn();
+        $dberror=0;
+    }
+} catch (Exception $e) {
+    $dberror=1;
 }
 
-if (isset($_SESSION["adduser"])){
-  if($_SESSION["adduser"]=="" || $result==1 || $dberror==1){
-    $errorMessages[] = "操作エラーです";
+if (isset($_SESSION["adduser"])) {
+    if ($_SESSION["adduser"]=="" || $result==1 || $dberror==1) {
+        $errorMessages[] = "操作エラーです";
+        $_SESSION["errorMessages"]=$errorMessages;
+        header('Location: /operate_error.php');
+        exit;
+    } else {
+        $sql = $pdo->prepare("INSERT INTO login (username,pass,addcard,editcard,sendnotice,viewexit,shutdown,edituser) VALUES (?,?,?,?,?,?,?,?)");
+        $sql->bindValue(1, $_SESSION["adduser"]);
+        $sql->bindValue(2, $_SESSION["addpassword"]);
+        $sql->bindValue(3, $_SESSION["permission"]["addcard"]);
+        $sql->bindValue(4, $_SESSION["permission"]["editcard"]);
+        $sql->bindValue(5, $_SESSION["permission"]["sendnotice"]);
+        $sql->bindValue(6, $_SESSION["permission"]["viewexit"]);
+        $sql->bindValue(7, $_SESSION["permission"]["shutdown"]);
+        $sql->bindValue(8, $_SESSION["permission"]["edituser"]);
+        $sql->execute();
+    }
+} else {
+    $errorMessages[] = "操作エラーです。";
     $_SESSION["errorMessages"]=$errorMessages;
     header('Location: /operate_error.php');
     exit;
-  }else{
-
-  $sql = $pdo->prepare("INSERT INTO login (username,pass,addcard,editcard,sendnotice,viewexit,shutdown,edituser) VALUES (?,?,?,?,?,?,?,?)");
-  $sql->bindValue(1,$_SESSION["adduser"]);
-  $sql->bindValue(2,$_SESSION["addpassword"]);
-  $sql->bindValue(3,$_SESSION["permission"]["addcard"]);
-  $sql->bindValue(4,$_SESSION["permission"]["editcard"]);
-  $sql->bindValue(5,$_SESSION["permission"]["sendnotice"]);
-  $sql->bindValue(6,$_SESSION["permission"]["viewexit"]);
-  $sql->bindValue(7,$_SESSION["permission"]["shutdown"]);
-  $sql->bindValue(8,$_SESSION["permission"]["edituser"]);
-  $sql->execute();
-}
-}else{
-  $errorMessages[] = "操作エラーです。";
-  $_SESSION["errorMessages"]=$errorMessages;
-  header('Location: /operate_error.php');
-  exit;
 }
 unset($_SESSION["adduser_status"]);
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>入退室管理システム</title>
-    <link rel="stylesheet" href="/material/material.min.css">
-    <script src="/material/material.min.js"></script>
-    <link rel="stylesheet" href="/material/iconfont/material-icons.css">
-    <link rel="stylesheet" type="text/css" href="/custom.css">
-  </head>
-  <body>
 
-    <!-- Always shows a header, even in smaller screens. -->
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <!-- Title -->
-          <span class="mdl-layout-title">管理ユーザー新規登録ウィザード</span>
-          <!-- Add spacer, to align navigation to the right -->
-          <div class="mdl-layout-spacer"></div>
-          <!-- Navigation. We hide it in small screens. -->
-          <nav class="mdl-navigation mdl-layout--large-screen-only">
-          </nav>
-          <div class="logout">
+<head>
+  <title>入退室管理システム</title>
+  <link rel="stylesheet" href="/material/material.min.css">
+  <script src="/material/material.min.js"></script>
+  <link rel="stylesheet" href="/material/iconfont/material-icons.css">
+  <link rel="stylesheet" type="text/css" href="/custom.css">
+</head>
+
+<body>
+  <!-- Always shows a header, even in smaller screens. -->
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <!-- Title -->
+        <span class="mdl-layout-title">管理ユーザー新規登録ウィザード</span>
+        <!-- Add spacer, to align navigation to the right -->
+        <div class="mdl-layout-spacer"></div>
+        <!-- Navigation. We hide it in small screens. -->
+        <nav class="mdl-navigation mdl-layout--large-screen-only">
+        </nav>
+        <div class="logout">
           <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onclick="location.href='/adminuser/adminuser.php'">
-             キャンセル
+            キャンセル
           </button>
+        </div>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <!-- Your content goes here -->
+        <div class="c-add-card mdl-card mdl-shadow--4dp">
+          <div class="mdl-card__supporting-text">
+            完了
+          </div>
+          <div class="mdl-card__supporting-text">
+            登録が完了しました
+            <br>
+            <div class="c-r-button">
+              <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" type="button" onclick="location.href='../adminuser.php'">
+                メニューへ戻る
+              </button>
+            </div>
           </div>
         </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="page-content"><!-- Your content goes here -->
-
-
-<div class="c-add-card mdl-card mdl-shadow--4dp">
-  <div class="mdl-card__supporting-text">
-    完了
+      </div>
+    </main>
   </div>
-  <div class="mdl-card__supporting-text">
-    登録が完了しました
-  <br>
-  <div class="c-r-button">
-  <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" type="button" onclick="location.href='../adminuser.php'">
-     メニューへ戻る
-  </button>
-</div>
-</div>
-</div>
-</div>
-      </main>
-    </div>
+</body>
 
-  </body>
 </html>
